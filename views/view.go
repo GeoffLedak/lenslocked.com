207fa03ea7ad21441d475cc959daff24c397ebe5
Geoff Ledak <geoffledak@gmail.com>
2018-10-15 16:22:35 -0600

    Saving

--- views/view.go
+++ views/view.go
@@ -2,9 +2,9 @@ package views
 
 import "html/template"
 
-func NewView(files ...string) *View {
+func NewView(layout string, files ...string) *View {
 
-	files = append(files, "views/layouts/footer.gohtml")
+	files = append(files, "views/layouts/footer.gohtml", "views/layouts/bootstrap.gohtml")
 	t, err := template.ParseFiles(files...)
 
 	if err != nil {
@@ -13,10 +13,12 @@ func NewView(files ...string) *View {
 
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
 
 }
 
 type View struct {
 	Template *template.Template
+	Layout   string
 }
