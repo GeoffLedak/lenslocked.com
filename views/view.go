963c5a5648c288ccc527fd759959b225dbaa1f82
Geoff Ledak <geoffledak@gmail.com>
2018-10-22 14:15:02 -0600

    Finished 6.5.4 - need to do 6.6 Exercises

--- views/view.go
+++ views/view.go
@@ -2,6 +2,7 @@ package views
 
 import (
 	"html/template"
+	"net/http"
 	"path/filepath"
 )
 
@@ -37,4 +38,8 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
