93a9d5cb1f751dcb18519a0952c8ab42a0acfe3f
Geoff Ledak <geoffledak@gmail.com>
2019-02-04 16:28:09 -0700

    Finished 8.3.5 - beginning 8.3.6

--- exp/main.go
+++ exp/main.go
@@ -22,30 +22,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
 
-	// The first version w/out the ID
-	_, err = db.Exec(`
-	  INSERT INTO users(name, email)
-	  VALUES($1, $2)`,
-		"Jon Calhoun", "jon@calhoun.io")
-	if err != nil {
-		panic(err)
-	}
-
-	// The second version that returns the ID
 	var id int
-	row := db.QueryRow(`
-		INSERT INTO users(name, email)
-		VALUES($1, $2) RETURNING id`,
-		"Jon2 Calhoun2", "jon2@calhoun2.io")
-	err = row.Scan(&id)
+	var name, email string
+	rows, err := db.Query(`
+    SELECT id, name, email
+    FROM users
+    WHERE email = $1
+    OR ID > $2`,
+		"jon@calhoun.io", 3)
 	if err != nil {
 		panic(err)
 	}
 
+	for rows.Next() {
+		rows.Scan(&id, &name, &email)
+		fmt.Println("ID:", id, "Name:", name, "Email:", email)
+	}
 	db.Close()
 }
