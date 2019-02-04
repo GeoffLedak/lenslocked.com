b3ba56a5ad719237fd0f3a5a2b189e0e0fba20b9
Geoff Ledak <geoffledak@gmail.com>
2019-02-04 16:10:57 -0700

    Finished 8.3.3 - beginning 8.3.4

--- exp/main.go
+++ exp/main.go
@@ -1,25 +1,51 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "lenslocked_dev"
 )
 
 func main() {
-
-	t, err := template.ParseFiles("hello.gohtml")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"dbname=%s sslmode=disable",
+		host, port, user, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
 
-	data := struct {
-		Name string
-	}{"<script>alert('Howdy!');</script>"}
-
-	err = t.Execute(os.Stdout, data)
-
+	// The first version w/out the ID
+	_, err = db.Exec(`
+	  INSERT INTO users(name, email)
+	  VALUES($1, $2)`,
+		"Jon Calhoun", "jon@calhoun.io")
 	if err != nil {
 		panic(err)
 	}
 
+	// The second version that returns the ID
+	var id int
+	row := db.QueryRow(`
+		INSERT INTO users(name, email)
+		VALUES($1, $2) RETURNING id`,
+		"Jon2 Calhoun2", "jon2@calhoun2.io")
+	err = row.Scan(&id)
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
 }
