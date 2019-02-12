620aa5beb5db02b3a0a6b4ab906c8bf4b4ea4388
Geoff Ledak <geoffledak@gmail.com>
2019-02-12 14:07:17 -0700

    Finished 9.7.2 - Beginning 9.7.3

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"lenslocked.com/controllers"
+	"lenslocked.com/models"
 
 	"github.com/gorilla/mux"
 )
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = ""
+	dbname   = "lenslocked_dev"
+)
+
 func main() {
+	// Create a DB connection string and then use it to create our model services.
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+	us.AutoMigrate()
+
 	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers()
 
