f4713fe9a4c9471be2fead01f2cb12cf36ea322b
Geoff Ledak <geoffledak@gmail.com>
2019-02-20 15:14:51 -0700

    Finished 10.5.4 - Beginning 10.6 Exercises

--- controllers/users.go
+++ controllers/users.go
@@ -69,10 +69,23 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 //
 // POST /login
 func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
+
 	form := LoginForm{}
+
 	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
-	// We will eventually do something to see if the
-	// information provided is correct.
+
+	user, err := u.us.Authenticate(form.Email, form.Password)
+
+	switch err {
+	case models.ErrNotFound:
+		fmt.Fprintln(w, "Invalid email address.")
+	case models.ErrInvalidPassword:
+		fmt.Fprintln(w, "Invalid password provided.")
+	case nil:
+		fmt.Fprintln(w, user)
+	default:
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
