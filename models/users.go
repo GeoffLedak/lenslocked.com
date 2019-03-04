2926faf93b3fdb20ccd15c825a3b7925c851a6c3
Geoff Ledak <geoffledak@gmail.com>
2019-03-04 16:22:33 -0700

    Finished 11.7.2 - Beginning 11.7.3

--- models/users.go
+++ models/users.go
@@ -3,6 +3,8 @@ package models
 import (
 	"errors"
 
+	"lenslocked.com/rand"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"golang.org/x/crypto/bcrypt"
@@ -27,6 +29,8 @@ type User struct {
 	Email        string `gorm:"not null;unique_index"`
 	Password     string `gorm:"-"`
 	PasswordHash string `gorm:"not null"`
+	Remember     string `gorm:"-"`
+	RememberHash string `gorm:"not null;unique_index"`
 }
 
 type UserService struct {
@@ -123,6 +127,15 @@ func (us *UserService) Create(user *User) error {
 
 	user.PasswordHash = string(hashedBytes)
 	user.Password = ""
+
+	if user.Remember == "" {
+		token, err := rand.RememberToken()
+		if err != nil {
+			return err
+		}
+		user.Remember = token
+	}
+	// TODO: Hash the token and set it on user.RememberHash
 
 	return us.db.Create(user).Error
 }
