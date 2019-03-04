67364283afbde544248a401d970d08569fe90a60
Geoff Ledak <geoffledak@gmail.com>
2019-03-04 16:01:52 -0700

    Finished 11.6.3 - Beginning 11.7

--- exp/main.go
+++ exp/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 
-	"lenslocked.com/rand"
+	"lenslocked.com/hash"
 )
 
 func main() {
-	fmt.Println(rand.String(10))
-	fmt.Println(rand.RememberToken())
+	hmac := hash.NewHMAC("my-secret-key")
+	// This should print out:
+	//   4waUFc1cnuxoM2oUOJfpGZLGP1asj35y7teuweSFgPY=
+	fmt.Println(hmac.Hash("this is my string to hash"))
 }
