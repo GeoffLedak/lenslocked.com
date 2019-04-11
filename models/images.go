e612ccae29852478dea4ebbaa9709bb414b7e4e8
Geoff Ledak <geoffledak@gmail.com>
2019-04-10 19:08:40 -0600

    Added ToSlash so it works on Windows

--- models/images.go
+++ models/images.go
@@ -26,7 +26,7 @@ func (i *Image) Path() string {
 func (i *Image) RelativePath() string {
 	// Convert the gallery ID to a string
 	galleryID := fmt.Sprintf("%v", i.GalleryID)
-	return filepath.Join("images", "galleries", galleryID, i.Filename)
+	return filepath.ToSlash(filepath.Join("images", "galleries", galleryID, i.Filename))
 }
 
 type ImageService interface {
